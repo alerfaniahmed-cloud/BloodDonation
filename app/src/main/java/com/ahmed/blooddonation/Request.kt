81d9083e2a5bedcd0f12bcf929e56b7bc7483e05
alerfaniahmed-cloud <alerfaniahmed@gmail.com>
2026-07-16 00:56:51 +0300

    Create Request.kt

---
+++ app/src/main/java/com/ahmed/blooddonation/Request.kt
@@ -0,0 +1,12 @@
+package com.ahmed.blooddonation
+
+data class Request(
+    var id: String = "",
+    var bloodType: String = "",
+    var city: String = "",
+    var urgency: String = "",
+    var contactPhone: String = "",
+    var notes: String = "",
+    var requesterName: String = "",
+    var timestamp: Long = 0L
+)
