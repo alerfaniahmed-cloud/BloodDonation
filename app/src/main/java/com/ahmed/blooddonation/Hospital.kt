39fddb84d62f5b0a1abfa9c33d22db19ec96aecf
alerfaniahmed-cloud <alerfaniahmed@gmail.com>
2026-07-16 08:05:54 +0300

    Update Hospital.kt

--- app/src/main/java/com/ahmed/blooddonation/Hospital.kt
+++ app/src/main/java/com/ahmed/blooddonation/Hospital.kt
@@ -3,5 +3,7 @@ package com.ahmed.blooddonation
 data class Hospital(
     val name: String = "",
     val city: String = "",
-    val phone: String = ""
+    val phone: String = "",
+    val lat: Double = 0.0,
+    val lng: Double = 0.0
 )
