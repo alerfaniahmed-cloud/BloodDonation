9b1e9bebf788b16ea85c01eb812eb3463b4756bf
alerfaniahmed-cloud <alerfaniahmed@gmail.com>
2026-07-17 20:08:15 +0300

    Update MainActivity.kt

--- app/src/main/java/com/ahmed/blooddonation/app/src/main/java/com/ahmed/blooddonation/MainActivity.kt
+++ app/src/main/java/com/ahmed/blooddonation/app/src/main/java/com/ahmed/blooddonation/MainActivity.kt
@@ -56,6 +56,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         setContentView(R.layout.activity_main)
 
         db = FirebaseFirestore.getInstance()
