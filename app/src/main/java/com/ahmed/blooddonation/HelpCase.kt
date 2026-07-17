0f05a43f35943c0d65dfb4898d5a4fe86ae4ac0d
alerfaniahmed-cloud <alerfaniahmed@gmail.com>
2026-07-18 00:38:40 +0300

    Create HelpCase.kt

---
+++ app/src/main/java/com/ahmed/blooddonation/HelpCase.kt
@@ -0,0 +1,15 @@
+package com.ahmed.blooddonation
+
+data class HelpCase(
+    var id: String = "",
+    val title: String = "",
+    val description: String = "",
+    val category: String = "",
+    val amountNeeded: String = "",
+    val city: String = "",
+    val charityName: String = "",
+    val charityContact: String = "",
+    val requesterName: String = "",
+    val userId: String = "",
+    val timestamp: Long = 0L
+)
