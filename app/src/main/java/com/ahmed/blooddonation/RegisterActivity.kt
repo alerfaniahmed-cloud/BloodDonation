cc18626434376f1241407a611f4bfe8e59e46cb9
alerfaniahmed-cloud <alerfaniahmed@gmail.com>
2026-07-17 20:06:36 +0300

    Update RegisterActivity.kt

--- app/src/main/java/com/ahmed/blooddonation/RegisterActivity.kt
+++ app/src/main/java/com/ahmed/blooddonation/RegisterActivity.kt
@@ -19,6 +19,7 @@ class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         setContentView(R.layout.activity_register)
 
         auth = FirebaseAuth.getInstance()
