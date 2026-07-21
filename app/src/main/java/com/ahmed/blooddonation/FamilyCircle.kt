bdee93de3f2b1d139fc28de3191b7fdb8f53f21f
alerfaniahmed-cloud <alerfaniahmed@gmail.com>
2026-07-21 17:22:50 +0300

    Update FamilyCircle.kt

--- app/src/main/java/com/ahmed/blooddonation/FamilyCircle.kt
+++ app/src/main/java/com/ahmed/blooddonation/FamilyCircle.kt
@@ -1,1 +1,17 @@
+package com.ahmed.blooddonation
 
+data class FamilyCircle(
+    var id: String = "",
+    val name: String = "",
+    val inviteCode: String = "",
+    val creatorId: String = "",
+    val timestamp: Long = 0L
+)
+
+data class CircleMemberInfo(
+    val userId: String = "",
+    val name: String = "",
+    val bloodType: String = "",
+    val phone: String = "",
+    val city: String = ""
+)
